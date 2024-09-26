3a31ee4c6cf1c7551d4b1e80d4f74d595cd88828
mariagdc <mariagodoydelcastillo@gmail.com>
2024-09-26 17:43:49 -0300

    comparacion fechas

--- guia8.1/guia8.1/Models/Calendario.cs
+++ guia8.1/guia8.1/Models/Calendario.cs
@@ -8,44 +8,37 @@ using System.Threading.Tasks;
 
 namespace guia8._1.Models
 {
-    public  class Calendario
+    public class Calendario
     {
-        int CantidadFeriados {get;}
-       ArrayList  feriados = new ArrayList();
-        //public Feriado this[DateTime dia]
-        //{
-        //    get
-        //    {
-        //        for (int n = 0; n < CantidadFeriados; n++)
-        //        {
-        //            if (dia != feriados[n].Fecha)
-        //            {
-        //                return (Feriado)feriados[n];
-        //            }
-        //        }
-        //        return null;
-        //    }
-        //}
-        public Feriado AgregarFeriado(DateTime dia, string descrip)
-        {
-            return null;
-        }
-
-        public Feriado this[DateTime dia] 
-        
+        int CantidadFeriados { get; }
+        ArrayList feriados = new ArrayList();
+        public Feriado this[DateTime dia]
         {
             get
-            { foreach(Feriado feriado in feriados)
+            {
+                for (int i = 0; i < CantidadFeriados; i++)
                 {
-                    if(feriado.Fecha.Year==dia.Year && feriado.Fecha.Month == dia.Month && feriado.Fecha.Day == dia.Fecha)
-                        {
 
-                        return feriado;
+                    Feriado f = feriados[i] as Feriado;
+                    if (f.Fecha.Year == dia.Year && f.Fecha.Month == dia.Month && f.Fecha.Day == dia.Day)
+                    {
+                        return f;
                     }
-                }return null;
-            
-            
+                    
+                }
+                return null;
             }
         }
+        public Feriado AgregarFeriado(DateTime dia, string descrip)
+        {
+           Feriado f = new Feriado( dia, descrip);
+
+            feriados.Add( f );
+            return f;
+        }
+
+
+
+
     }
 }
--- guia8.1/guia8.1/Models/PlanDePago.cs
+++ guia8.1/guia8.1/Models/PlanDePago.cs
@@ -8,5 +8,21 @@ namespace guia8._1.Models
 {
     internal class PlanDePago
     {
+        public double Monto { get; private set; }
+        public int CantCuotas {  get; private set; }
+        public DateTime FechaAlta { get; private set; }
+        public double MontoTotal { get; private set; }
+
+        public PlanDePago(double monto, int cantcuotas,
+             DateTime FechaAlta, Infractor desti, Calendario c)
+        {
+        
+        
+        
+        
+        
+        
+        }
+
     }
 }
